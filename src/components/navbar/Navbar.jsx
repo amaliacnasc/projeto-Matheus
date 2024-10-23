caf53874aef700db9814ffd1630bae3fc6fa6197
amaliacnasc <amaliacnasc@gmail.com>
2024-10-22 22:05:46 -0300

    adicionando link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,16 @@ import { Container, Nav, Navbar, Image } from 'react-bootstrap';
 import logo from '../../assets/img/ÍCONE.png';
 
 const NavbarComponent = () => {
+  const handleWhatsAppClick = () => {
+    const phoneNumber = '5581997399003'; // Número de telefone no formato internacional (sem o "-")
+    const message = 'Olá Matheus, gostaria de solicitar um orçamento.'; // Mensagem personalizada
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    
+    window.open(whatsappUrl, '_blank'); // Abre o link em uma nova aba
+  };
+
   return (
-    <Navbar expand='lg' >
+    <Navbar expand='lg'>
       <Container>
         <Navbar.Brand href="#home">
           <Image className='logo' src={logo} alt="Logo" />
@@ -13,9 +21,13 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='ms-auto nav-links'>
-            <Nav.Link className='fonte' href="#">Sobre</Nav.Link>
-            <Nav.Link className='fonte' href="#">Feedbacks</Nav.Link>
-            <Nav.Link className='fonte' href="#">Contato</Nav.Link>
+            <Nav.Link href="https://www.behance.net/theushmdesign" target="_blank" rel="noopener noreferrer" className="fonte">
+              Portfolio
+            </Nav.Link>
+            
+            <Nav.Link className='fonte' onClick={handleWhatsAppClick}>
+              Contato
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
